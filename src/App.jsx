2748f8954cc96f347cb7d2a9d654682274673141
Hossein Salari <hosein.salari81@gmail.com>
2024-02-19 19:59:48 +0330

    Using useReducer to manage notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
 import "./App.css";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import AddNewNote from "./components/AddNewNote";
 import NotesList from "./components/NotesList";
 import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
 
+function notesReducer(notes, { type, payload }) {
+  switch (type) {
+    case "add": {
+      return [...notes, payload];
+    }
+    case "delete": {
+      return notes.filter((note) => note.id !== payload);
+    }
+    case "complete": {
+      return notes.map((note) =>
+        note.id === payload ? { ...note, completed: !note.completed } : note
+      );
+    }
+    default:
+      throw new Error("unknown action" + type);
+  }
+}
+
 function App() {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [sortBy, setSortBy] = useState("latest");
 
-  const handleAddNote = (newNote) =>
-    setNotes((prevNotes) => [...prevNotes, newNote]);
+  const handleAddNote = (newNote) => {
+    // setNotes((prevNotes) => [...prevNotes, newNote]);
+    dispatch({ type: "add", payload: newNote });
+  };
 
-  const handleDeleteNote = (id) =>
-    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+  const handleDeleteNote = (id) => {
+    // setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+    dispatch({ type: "delete", payload: id });
+  };
 
   const handleCompleteNote = (e) => {
     const noteId = Number(e.target.value);
 
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note
-      )
-    );
+    // setNotes((prevNotes) =>
+    //   prevNotes.map((note) =>
+    //     note.id === noteId ? { ...note, completed: !note.completed } : note
+    //   )
+    // );
+
+    dispatch({ type: "complete", payload: noteId });
   };
 
   return (
